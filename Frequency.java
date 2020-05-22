9f3d86d352bd6c754d033c14dc8c7c423ad7c1b2
Shaikh Numaan <53473560+sknum@users.noreply.github.com>
2020-05-22 16:47:47 +0530

    Create Frequency.java

---
+++ Frequency.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Frequency   
+{  
+     public static void main(String[] args) {  
+	   Scanner sc= new Scanner(System.in);
+	   System.out.println("Enter the String");
+	   String str=sc.nextLine();
+        int[] freq = new int[str.length()];  
+        int i, j;
+        char string[] = str.toCharArray();  
+        for(i = 0; i <str.length(); i++) {  
+            freq[i] = 1;  
+            for(j = i+1; j <str.length(); j++) {  
+                if(string[i] == string[j]) {  
+                    freq[i]++;  
+                      
+                    //Set string[j] to 0 to avoid printing visited character  
+                    string[j] = '0';  
+                }  
+            }  
+        }  
+        System.out.println("Characters and their corresponding frequencies");  
+        for(i = 0; i <freq.length; i++) {  
+            if(string[i] != ' ' && string[i] != '0')  
+                System.out.println(string[i] + "-" + freq[i]);  
+        }  
+    }  
+}  
